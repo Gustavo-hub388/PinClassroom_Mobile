3ac6b14454f773eaf2dbf524857e3c196e8a7338
Gustavo_Flamenco <flemish.vladd3030@gmail.com>
2019-10-24 12:36:24 -0600

    Reedireccionamiento para registro

--- app/src/main/java/com/example/pinclassroom/Login/LoginInterface.java
+++ app/src/main/java/com/example/pinclassroom/Login/LoginInterface.java
@@ -1,37 +1,53 @@
 package com.example.pinclassroom.Login;
 
 public interface LoginInterface {
+    /*Se declaran las 4 interfaces principales
+    *View, Presenter, Model, Tasklistener
+     */
+
+    // Esto es todoo lo que va a controlar la vista
     interface View {
-        void disableInputs();
-        void enableInputs();
+        //Metodos
 
-        void showProgress();
-        void hideProgress();
+        void disableInputs();//desactivar los componentes despues de dar inicio una accion
+        void enableInputs();//activar los componentes despues terminar una accion
 
-        void handleLogin();
+        void showProgress();//Mostrar un progreso mientras cuando envia los datos al presentador
+        void hideProgress();//ocultar el progreso
 
-        boolean isValidEmailLogin();
-        boolean isValidPasswordLogin();
+        void handleLogin();//se encarga de que todoo vaya bien durante la accion
+
+        boolean isValidEmailLogin();//Validar el correo
+        boolean isValidPasswordLogin();//validar la contraseña
 
         void onLogin();
         void onError(String error);
     }
 
+    // Se encargara de trabajar con la vista de todos lo que se muestre
     interface Presenter {
-        void onDestroy();
+        //Metodos
+        void onDestroy();//elimina la vista todoo los residuos innecesarios
 
-        void toLogin(String emailLogin, String passwordLogin);
+        void toLogin(String emailLogin, String passwordLogin);//envia la modelo el correo y contraseña
 
     }
 
+    /*se encargara de verificar si los datos dados por el presentador son validos o no
+     * Y le da aviso al tasklistener
+     */
     interface Model {
-        void doLogin (String emailLogin, String passwordLogin);
+        //Metodo
+        void doLogin (String emailLogin, String passwordLogin);//pasamos los datos que verificara
 
     }
+
+    //Le da aviso al Presentador si son correctos los datos o no
 
     interface TaskListener{
-        void onSucess();
-        void onError(String error);
+        //Metodos
+        void onSucess();//dice que si estuvo correcto todoo
+        void onError(String error);//dice si huvo problemas
 
     }
 }
